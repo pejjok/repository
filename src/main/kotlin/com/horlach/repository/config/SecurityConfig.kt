2aeb27c53d1517f3c9a55c7ad892acf683cd8c40
Sasha Horlach <2018gorlach@gmail.com>
2026-06-02 14:18:28 +0300

    feat: implementing cors

--- src/main/kotlin/com/horlach/repository/config/SecurityConfig.kt
+++ src/main/kotlin/com/horlach/repository/config/SecurityConfig.kt
@@ -5,6 +5,7 @@ import com.horlach.repository.repositories.UserRepository
 import com.horlach.repository.security.JwtAuthFilter
 import com.horlach.repository.security.UserDetailsServiceImpl
 import com.horlach.repository.services.AuthService
+import org.hibernate.boot.jaxb.Origin
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.CommandLineRunner
 import org.springframework.context.annotation.Bean
@@ -21,6 +22,9 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import java.time.Instant
 
 @Configuration
@@ -32,11 +36,13 @@ class SecurityConfig {
     }
 
     @Bean
-    fun securityFilterChain(http: HttpSecurity, jwtAuthFilter: JwtAuthFilter): SecurityFilterChain? {
+    fun securityFilterChain(http: HttpSecurity, jwtAuthFilter: JwtAuthFilter, @Value("\${cors.origin}") origin: String): SecurityFilterChain? {
         http
             .authorizeHttpRequests { auth -> auth
                 .requestMatchers("/api/v1/auth/**").permitAll()
                 .requestMatchers(HttpMethod.GET,"/api/v1/works/**","/api/v1/work-files/**","/api/v1/specialties/**", "/api/v1/groups/**", "/api/v1/users/**")
+                .hasRole(UserRole.ROLE_USER.withoutPrefix)
+                .requestMatchers(HttpMethod.PATCH, "/api/v1/users") // for changing name
                 .hasRole(UserRole.ROLE_USER.withoutPrefix)
                 .requestMatchers(HttpMethod.POST,"/api/v1/work-files/*/request")
                 .hasRole(UserRole.ROLE_USER.withoutPrefix)
@@ -44,13 +50,13 @@ class SecurityConfig {
                 .hasRole(UserRole.ROLE_ADMIN.withoutPrefix)
                 .requestMatchers("/api/v1/works/**","/api/v1/work-files/**")
                 .hasRole(UserRole.ROLE_SUPERVISOR.withoutPrefix)
-                .requestMatchers("/api/v1/specialties/**", "/api/v1/groups/**", "/api/v1/users/**")
+                .requestMatchers("/api/v1/specialties/**", "/api/v1/groups/**", "/api/v1/users/**", "/api/v1/statistic/**")
                 .hasRole(UserRole.ROLE_ADMIN.withoutPrefix)
                 .anyRequest().authenticated()
             }
             .sessionManagement { session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
             .csrf { csrf -> csrf.disable() }
-            .cors { cors -> cors.disable() }
+            .cors { cors -> cors.configurationSource(corsConfigurationSource(origin)) }
             .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter::class.java)
         return http.build()
     }
@@ -93,5 +99,18 @@ class SecurityConfig {
             )
             userRepository.save(admin)
         }
+    }
+
+
+    @Bean
+    fun corsConfigurationSource(origin: String): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.setAllowedOriginPatterns(listOf("*"))
+        configuration.setAllowedMethods(listOf("GET", "POST", "PUT", "DELETE", "OPTIONS"))
+        configuration.setAllowedHeaders(listOf("*"))
+        configuration.setAllowCredentials(true)
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
     }
 }
